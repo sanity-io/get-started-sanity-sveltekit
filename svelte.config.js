8d977a06ad0565d916252cba940013af4df33cca
Mathias Picker <mattis123@live.no>
2022-08-30 14:31:43 +0200

    Migrate to latest svelte-kit api

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-node';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  }
+	preprocess: [preprocess()],
+	kit: {
+		adapter: adapter(),
+	}
 };
 
 export default config;
